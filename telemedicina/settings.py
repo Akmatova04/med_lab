d35fcf151f04500f1211c776d8adf2a4b373d55a
AsemaKen <asemaflex@gmail.com>
2025-11-04 04:25:07 -0600

    Fix project structure for Render

--- telemedicina/settings.py
+++ telemedicina/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     # СИЗДИН ТИРКЕМЕЛЕР
     'main',
     'whitenoise.runserver_nostatic',
-    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
@@ -88,9 +88,30 @@ WSGI_APPLICATION = 'telemedicina.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+# settings.py
+
+import os
+import dj_database_url
+
+# ... (сиздин калган setting'дериңиз)
+
+# RENDER'ДЕ ИШТЕП ЖАТКАНЫН ТЕКШЕРҮҮ
+if 'RENDER' in os.environ:
+    # Render'де PostgreSQL'ди колдонобуз
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # Компьютерде (локально) SQLite'ты колдонобуз
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
